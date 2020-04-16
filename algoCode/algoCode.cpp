2d73161937819ae986cd0fc99f6bf8428416ee99
gai <gaih@outlook.com>
2020-04-16 23:14:32 +0800

    翻转字符串里的单词 内存占用有些高 还有待优化

--- algoCode/algoCode.cpp
+++ algoCode/algoCode.cpp
@@ -10,7 +10,8 @@ int engthOfLongestSubstring(string);
 bool checkInclusion(string, string);
 string longestCommonPrefix(vector<string>&);
 vector<int> twoSum(vector<int>&, int);
-string multiply(string, string );
+string multiply(string, string);
+string reverseWords(string);
 int main()
 {
 	/*string s = "ytuwrqjjkljk";
@@ -39,10 +40,16 @@ int main()
 	//int sum = trap(height);
 	//cout << sum << endl;
 
-	string num1 = "100";
-	string num2 = "100";
-	string sum =multiply(num1, num2);
-	cout << sum << endl;
+	//string num1 = "100";
+	//string num2 = "100";
+	//string sum =multiply(num1, num2);
+	//cout << sum << endl;
+
+	string str = "we are the    world!";
+	string s = reverseWords(str);
+	cout << s << endl;
+
+
 	return 0;
 }
 //leetcode 1 两数之和
@@ -234,4 +241,53 @@ string multiply(string num1, string num2) {
 
 	}
 	return res;
+}
+
+
+//翻转字符串里的单词
+
+string reverseWords(string s) {
+	vector<string> sList;
+	string sp = " ";
+	int pos2 = s.find(sp);
+	int pos1 = 0;
+	while (string::npos != pos2)
+	{
+		int l = pos2 - pos1;
+		string c = s.substr(pos1, l);
+		if (c != "") {
+			sList.push_back(c);
+		}
+		pos1 = pos2 + sp.size();
+		pos2 = s.find(sp, pos1);
+	}
+	if (pos1 != s.length()) {
+		sList.push_back(s.substr(pos1));
+	}
+	int num = sList.size() / 2;
+	int m = sList.size() - 1;
+	for (int i = 0; i < num; i++)
+	{
+		string temp = sList[i];
+		sList[i] = sList[m - i];
+		sList[m - i] = temp;
+	}
+
+	string res = "";
+	int count = sList.size();
+	for (int i = 0; i < count; i++)
+	{
+		if (i == (count - 1)) {
+
+			res += sList[i];
+		}
+		else
+		{
+
+			res += sList[i] + " ";
+		}
+
+	}
+	return res;
+
 }
