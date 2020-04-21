91566f88ed019e9b8ce99b5ad85bb9f14af9fd80
gai <gaih@outlook.com>
2020-04-22 02:39:29 +0800

    调整结构

---
+++ algoCode/algoString.h
@@ -0,0 +1,29 @@
+﻿// algoCode.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+#include <vector>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <cstring>
+#include <algorithm>
+
+
+using namespace std;
+// TODO: 在此处引用程序需要的其他标头。
+int trap(vector<int>&);
+int engthOfLongestSubstring(string);
+bool checkInclusion(string, string);
+string longestCommonPrefix(vector<string>&);
+vector<int> twoSum(vector<int>&, int);
+string multiply(string, string);
+string reverseWords(string);
+string simplifyPath(string);
+vector<string> restoreIpAddresses(string);
+vector<vector<int>> threeSum(vector<int>&);
+int maxAreaOfIsland(vector<vector<int>>&);
+int dfs(vector<vector<int>>&, int, int, int, int);
+int search(vector<int>&, int);
+
