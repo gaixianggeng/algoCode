341929bedca034b955d3315448ee4792fda7dde0
gaixiaotongxue <gaih@outlook.com>
2020-05-16 23:44:36 +0800

    二叉树最近公共祖先

--- algoCode/listTree.cpp
+++ algoCode/listTree.cpp
@@ -284,7 +284,24 @@ ListNode* merge2Lists(ListNode *h1,ListNode *h2) {
 
 }
 //二叉树最近公共祖先
+//递归问题
 TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+	if(root == NULL){
+		return NULL;
+	}
+	if (root == p || root == q){
+		return root;
+	}
+	TreeNode* left = lowestCommonAncestor(root->left,p,q);
+	TreeNode* right= lowestCommonAncestor(root->right,p,q);
+	if (left == NULL){
+		return right;
+	}
+	if(right == NULL){
+		return left;
+	}
+	return root;
+
 }
 
 
