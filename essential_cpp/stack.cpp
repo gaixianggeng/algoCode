7b2bb8fb0d5b39aa9e9b5ea34307cea3871cfcd7
gaixg <gaixg94@gmail.com>
2023-10-09 02:03:24 +0800

    add stack

--- essential_cpp/stack.cpp
+++ essential_cpp/stack.cpp
@@ -1,2 +1,23 @@
 #include "stack.h"
-bool Stack::empty() const { return _stack.empty(); }
+
+bool Stack::push(const string &elem) {
+  if (full())
+    return false;
+  _stack.push_back(elem);
+  return true;
+}
+
+bool Stack::pop(string &elem) {
+  if (empty())
+    return false;
+  elem = _stack.back();
+  _stack.pop_back();
+  return true;
+}
+
+bool Stack::peek(string &elem) {
+  if (empty())
+    return false;
+  elem = _stack.back();
+  return true;
+}
--- essential_cpp/stack.h
+++ essential_cpp/stack.h
@@ -7,8 +7,8 @@ public:
   bool push(const string &);
   bool pop(string &elem);
   bool peek(string &elem);
-  bool empty() const;
-  bool full();
+  bool empty() const { return _stack.empty(); }
+  bool full() const { return _stack.size() == _stack.max_size(); };
   int size() { return _stack.size(); }
 
 private:
